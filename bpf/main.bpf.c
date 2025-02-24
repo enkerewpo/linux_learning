b5f231243344a182d6f8793c2c6e46e41ebf3ddf
Yulong Han <wheatfox17@icloud.com>
2025-02-24 20:54:58 +0800

    Enhance BPF Makefile and add hello world example program

--- bpf/main.bpf.c
+++ bpf/main.bpf.c
@@ -1,10 +1,15 @@
-// example.bpf.c
 #include "vmlinux.h"
 #include <bpf/bpf_helpers.h>
 
 SEC("tracepoint/syscalls/sys_enter_openat")
 int handle_openat(struct trace_event_raw_sys_enter *ctx) {
   bpf_printk("Openat syscall detected!\n");
+  return 0;
+}
+
+SEC("tracepoint/syscalls/sys_enter_execve")
+int hello(struct trace_event_raw_sys_enter *ctx) {
+  bpf_printk("Hello, BPF World!\n");
   return 0;
 }
 
